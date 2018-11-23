23eddb745686c648b5f85d1765977e0b6ae1e224
Kiet Vo <geforce2409@gmail.com>
2018-11-23 19:40:07 +0700

    change location position (part 1)

--- MyImage/Map3D/assets/js/main.js
+++ MyImage/Map3D/assets/js/main.js
@@ -261,10 +261,10 @@ var createScene = function() {
         locParking.position= new BABYLON.Vector3(PosLocation[1].x + 150, PosLocation[1].z, PosLocation[1].y - 200);
         locNationalLeague.position= new BABYLON.Vector3(PosLocation[2].x + 150, PosLocation[2].z, PosLocation[2].y - 220);
         locKippaxLake.position= new BABYLON.Vector3(PosLocation[3].x + 70, PosLocation[3].z, PosLocation[3].y - 300);
-        locStadiumSportsPhysiotherapy.position= new BABYLON.Vector3(PosLocation[4].x + 50, PosLocation[4].z, PosLocation[4].y - 300);
-        locGateACricket.position= new BABYLON.Vector3(PosLocation[5].x + 150, PosLocation[5].z, PosLocation[5].y - 200);
-        locSydneyCricketGround.position= new BABYLON.Vector3(PosLocation[6].x + 150, PosLocation[6].z, PosLocation[6].y);
-        locStadiumForecourt.position= new BABYLON.Vector3(PosLocation[7].x + 100, PosLocation[7].z, PosLocation[7].y - 200);
+        locStadiumSportsPhysiotherapy.position= new BABYLON.Vector3(-800, -10, 1450);
+        locGateACricket.position= new BABYLON.Vector3(-1150, -10, 400);
+        locSydneyCricketGround.position= new BABYLON.Vector3(-800, -10, 150);
+        locStadiumForecourt.position= new BABYLON.Vector3(-1200, -10, 1050);
     }, 5000);
 
     // Đặt các thuộc tính cho việc vẽ Route
@@ -335,106 +335,6 @@ var createScene = function() {
     meshTask.onSuccess = function (task) {
         m = task.loadedMeshes;
         m[0].position = new BABYLON.Vector3(-900, -50, 800);
-        for(var i = 0; i < m.length; i++) { // Set material cho các model dựa vào danh sách name và id của từng model
-
-            // /////////////////// CREATE MATERIAL FOR OBJECT ///////////////////
-            // m[i].material = new BABYLON.StandardMaterial("mat", scene);
-
-            // ///////////////////TREE TEXTURE/////////////// 
-            // if ((i >= 147 && i < 150) || i == 789) {                
-            //     m[i].material.diffuseColor = new BABYLON.Color3(0.182,0.9488,0.15); 
-            // } 
-
-            // ///////////////////CAR TEXTURE///////////////  
-            // if (i >= 724 && i < 730) {                
-            //     m[i].material.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());           
-            // }    
-
-            // ///////////////////FLAG ALLIANZ STADIUM TEXTURE/////////////// 
-            // if (i >= 64 && i < 68) {                
-            //     m[i].material.diffuseColor = new BABYLON.Color3(0.3, 0.3, 0.8);    
-            // }
-
-            // ///////////////////ALLIANZ GROUND TEXTURE///////////////   
-            // if (i == 635) {                         
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/d49dbff4cf5e1f3454dddc049942da5d3378ac6b/MyImage/testMini/Allianz_Fleild.jpg", scene);    
-            // }
-
-            // ///////////////////SYDNEY CRICKET GROUND TEXTURE/////////////// 
-            // if (i == 439) {                        
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/5ca60f119f14ff138829d3ada068cf5d.JPG", scene);
-            // }
-
-            // ///////////////////LAKE & SWIMMING POOL TEXTURE///////////////  
-            // if (i == 119 || i == 123 || i == 636 || (i >= 136 && i < 144)) {                         
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/d49dbff4cf5e1f3454dddc049942da5d3378ac6b/MyImage/testMini/319e89e8fd86b7f4a5ab254920712038.jpg", scene);
-            // }
-
-            // ///////////////////DOWN GROUND TEXTURE///////////////   
-            // if (i == 452 || i == 11 || i == 451) {                  
-            //     m[i].position.y = -50;       
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/d49dbff4cf5e1f3454dddc049942da5d3378ac6b/MyImage/testMini/TexturesCom_Grass0197_2_seamless_S.jpg", scene);
-            // }
-            // if (i == 209 || i == 440 ) {                  
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/d49dbff4cf5e1f3454dddc049942da5d3378ac6b/MyImage/testMini/06feee7a09df2f669e6f71a3d4f9ee2b.jpg", scene);
-            //     m[i].position.y = -30;  
-            // }
-
-            // //////////////////GATE A TEXTURE/////////////// 
-            // if ((i >= 459 && i < 469) || (i >= 337 && i < 339) || i == 334 || i == 379 || i == 372 || i == 477 || i == 281 || i == 491 || i == 76) {               
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/d49dbff4cf5e1f3454dddc049942da5d3378ac6b/MyImage/testMini/68ef63aaad90d63d4605c1b44edadfc0.jpg", scene);       
-            // }
-            // if (i == 397 || i == 406 || i == 304 || i == 294 || i == 466) {                         
-            //     m[i].material.diffuseColor = new BABYLON.Color3(13/255, 45/255, 18/255);       
-            // }   
-
-            // ///////////////////ALLIANZ STADIUM SEATS TEXTURE/////////////// 
-            // if ((i >= 200 && i < 202) || i == 643 || i == 556 || i == 648 || i == 550) {            
-            //     m[i].material.diffuseColor = new BABYLON.Color3(0, 229/255, 238/255);          
-            // }
-                   
-            // ///////////////////SYDNEY CRICKET GROUND SEATS TEXTURE///////////////   
-            // if ((i >= 469 && i < 471) || (i >= 475 && i < 477) || (i >= 482 && i < 484) || i == 472 
-            // || i == 480 || i == 485 || i == 275 || i == 284 || i == 278 || i == 478) {           
-            //     m[i].material.diffuseColor = new BABYLON.Color3(0, 229/255, 238/255);          
-            // }
-
-            // ///////////////////////// WALK GROUND ///////////////////////////   
-            // if (i == 644 || i == 8) {               
-            //     if (i == 644)
-            //         m[i].position.y = -60;   
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/d49dbff4cf5e1f3454dddc049942da5d3378ac6b/MyImage/testMini/2d628b2f5cedba319c081744bf92f344.jpg", scene);              
-            // }
-
-            // ///////////////////////// HOUSE ROOF ///////////////////////////    
-            // if ((i >= 206 && i < 209) || (i >= 591 && i < 595)) {               
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/eb431a7787f33bbd2710b4d9e7076aeb.jpg", scene);              
-            // }
-            // if (i == 24) {              
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/64389da8647ed843c1f283430953d23c.jpg", scene);              
-            // }
-            // if (i == 56) {              
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/cement%20ice1.jpg", scene);              
-            // }
-
-            // ///////////////////////// GRASS TENNIS ///////////////////////////  
-            // if (i == 21) {               
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/Grass0153_30_S.jpg", scene);              
-            // }
-            // if (i == 78 || i == 88) {           
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/2a854fd2a1944ed815c0106114730282.jpg", scene);              
-            // }
-
-            // ///////////////////////// YARD TILES ///////////////////////////    
-            // if (i == 77 || i == 86) {              
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/b2cbf8640ebbd1f5217e6e280a096803.jpg", scene);              
-            // }
-
-            // ///////////////////////// ALLIANZ LOGO STADIUM ///////////////////////////  
-            // if (i == 598 || (i >= 651 && i < 654)) {               
-            //     m[i].material.diffuseTexture = new BABYLON.Texture("https://raw.githubusercontent.com/KiritoNguyen/My_Image/master/MyImage/testMini/Allianz_Stadium_logo.jpg", scene);              
-            // }
-        }
         BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
     }	
     
@@ -449,13 +349,6 @@ var createScene = function() {
     var options = new BABYLON.SceneOptimizerOptions(30, 500);
     options.addOptimization(new BABYLON.HardwareScalingOptimization(0, 2.5));
     var optimizer = new BABYLON.SceneOptimizer(scene, options);
-
-    //////////////// TANK MODEL ////////////////// => Load model xe tank
-    BABYLON.SceneLoader.ImportMesh("", "https://raw.githubusercontent.com/KiritoNguyen/My_Image/275d42382a314d6d0f7dfb27035bbc56b4431ef0/MyImage/Kiet/", "Tank.babylon", scene, function (newMeshes) {
-        tank = newMeshes[0];
-        tank.position = new BABYLON.Vector3(400, -40, -700);
-        tank.scaling = new BABYLON.Vector3(5, 5, 5);
-    });
     
     //////////////// HUD //////////////////    
     var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
@@ -848,39 +741,6 @@ var createScene = function() {
         }
     }
     document.getElementById("advOption").onclick = advOptionFunction;
-
-    /////////////////////////////////BUTTON MOVE TANK/////////////////////////////////////
-    var moveTankFunction = function() {
-        if(document.getElementById('moveTank').hidden == false) {
-            document.getElementById('moveTank').hidden = true;
-            setTimeout(function(){
-                document.getElementById('moveTank').hidden = false;
-                document.getElementById("cameraChange").disabled = false;
-            }, 12000);
-        }
-
-        var frameRate = 60;
-        var movein = new BABYLON.Animation("movein", "position", frameRate, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, 
-        BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);       
-        var movein_keys = []; 
-
-        movein_keys.push({
-            frame: 0,
-            value: new BABYLON.Vector3(-1350, -40, 500)
-        });
-        movein_keys.push({
-            frame: 6 * frameRate,
-            value: new BABYLON.Vector3(-1150, -40, -700)
-        });
-        movein_keys.push({
-            frame: 12 * frameRate,
-            value: new BABYLON.Vector3(-1050, -40, -1200)
-        });
-
-        movein.setKeys(movein_keys);
-        scene.beginDirectAnimation(tank, [movein], 0, 12 * frameRate, false);
-    }
-    document.getElementById("moveTank").onclick = moveTankFunction;
 
     /////////////////////////////////BUTTON DRAW ROUTE///////////////////////////////// => Xử lí sự kiện vẽ route cho button Draw Route
     var DrawMode = false;   //Kiểm tra chế độ vẽ ,mặc định false (không vẽ)
@@ -1410,18 +1270,6 @@ var createScene = function() {
                 lineSydneyCricketGroundt.linkOffsetY = -5;
             }, 10000)
         }
-
-        // Kiểm tra xem nếu user kích hoạt tính năng 'move Tank' thì sẽ không cho họ thay đổi chế độ camera đến khi tank dừng hẳn
-        if (document.getElementById('moveTank').hidden == true) {
-            camera.dispose();
-            document.getElementById("cameraChange").disabled = true;
-            document.getElementById("cameraChange").innerHTML = 'Free Camera';
-            camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(tank.position.x, tank.position.y + 100, tank.position.z + 400), scene);
-            camera.rotation.y = Math.PI;
-            camera.speed = 20;
-            camera.checkCollisions = true;
-            camera.attachControl(canvas, true);
-        }   
 
         // Kiểm tra xem nếu user kích hoạt tính năng 'Record' thì nếu user không di chuyển camera (xét cả vị trí và góc xoay) thì tọa độ đó sẽ không được lưu vào json
         if(Record)
